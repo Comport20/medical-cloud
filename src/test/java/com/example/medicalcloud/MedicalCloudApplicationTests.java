b149d7800009840b02d534ef47f84c7fc8363c97
Georg <program.0@yandex.ru>
2024-01-05 11:01:22 +0300

    change the test

--- src/test/java/com/example/medicalcloud/MedicalCloudApplicationTests.java
+++ src/test/java/com/example/medicalcloud/MedicalCloudApplicationTests.java
@@ -2,15 +2,14 @@ package com.example.medicalcloud;
 
 import com.example.medicalcloud.model.*;
 import com.example.medicalcloud.model.Records;
-import com.example.medicalcloud.repositories.DoctorRepository;
-import com.example.medicalcloud.repositories.PatientRepository;
-import com.example.medicalcloud.repositories.RecordRepository;
-import com.example.medicalcloud.repositories.SpecializationRepository;
+import com.example.medicalcloud.repositories.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @SpringBootTest
 class MedicalCloudApplicationTests {
@@ -22,19 +21,39 @@ class MedicalCloudApplicationTests {
     public DoctorRepository doctorRepository;
     @Autowired
     public SpecializationRepository specializationRepository;
+    @Autowired
+    public MedicalHistoryRepository medicalHistoryRepository;
+
     @Test
     void contextLoads() {
-        Specialization specialization = new Specialization("Терапевт");
-        Patient patient = new Patient("Алексей",123,123,1111111111,"@");
+
+        Specialization specialization = new Specialization("Direction");
+        Patient patient = new Patient("Алексей", 123345, 123, 1111111111, "patient@gmail.com");
         MedicalHistory medicalHistory = new MedicalHistory("больной");
         Disease disease = new Disease("disease");
-        Doctor doctor = new Doctor("name","login","password");
-        Records records = new Records(LocalDateTime.now(),100,RecordStatus.RECORDED);
-        patient.getRecords().add(records);
-        specialization.getDoctors().add(doctor);
-        medicalHistory.getDiseases().add(disease);
-        doctor.getRecords().add(records);
+        Doctor doctor = new Doctor("name", "login", "password");
+        Records records = new Records(LocalDateTime.of(
+                LocalDate.now(), LocalTime.of(
+                        LocalTime.now().getHour() - 1,
+                        LocalTime.now().getMinute(),
+                        LocalTime.now().getSecond(),
+                        LocalTime.now().getNano())),
+                100, RecordStatus.RECORDED);
+
+        specialization.addDoctor(doctor);
+        doctor.setSpecialization(specialization);
+        specializationRepository.save(specialization);
+
+        medicalHistory.addDisease(disease);
+        disease.addMedicalHistory(medicalHistory);
+        medicalHistoryRepository.save(medicalHistory);
+
+        patient.setMedicalHistory(medicalHistory);
+        patientRepository.save(patient);
+
+        records.setDoctor(doctor);
+        records.setPatient(patient);
+
         recordRepository.save(records);
     }
-
 }
